4d84e681f74489d63bd327de1301c892d4a49367
luuboiquan2007-source <luuboiquan2007@gmail.com>
2025-12-03 22:33:01 -0800

    Update Emotional_Generator.js

--- Emotional_Generator/Emotional_Generator.js
+++ Emotional_Generator/Emotional_Generator.js
@@ -153,9 +153,9 @@ class Shape {
 // ------------------------------
 function drawHeart(x,y,size){
   beginShape();
-  vertex(x,y);
-  bezierVertex(x-size/2, y-size/2, x-size, y+size/3, x,y);
-  bezierVertex(x+size, y+size/3, x+size/2, y-size/2, x,y);
+ vertex(x, y);
+  bezierVertex(x - size / 2, y - size / 2, x - size, y + size / 3, x, y + size);
+  bezierVertex(x + size, y + size / 3, x + size / 2, y - size / 2, x, y);
   endShape(CLOSE);
 }
 
@@ -273,3 +273,4 @@ class Sparkle {
 
 function mousePressed(){ pickNewEmotion(); }
 function windowResized(){ resizeCanvas(windowWidth, windowHeight); }
+
